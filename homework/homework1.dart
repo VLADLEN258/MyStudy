2eb97c3fbc6055206f1cb206c7a6e911206f80d4
Владислав <vladislav@A1278.local>
2022-05-18 14:02:40 +0300

    test commit

---
+++ homework/homework1.dart
@@ -0,0 +1,28 @@
+// gthfr
+void main (){
+  mane2();
+  variablenumber(12.3);
+}
+
+void variablenumber (var a){
+  if ((a % 1 == 0) & (a > 0)){
+    print("Число $a является натуральным числом");
+  }
+   else {
+    print("Число $a не является натуральным числом");
+  }
+}
+
+
+
+void mane2(){
+print('Hede');
+}
+
+void mane3(){
+
+}
+
+void mane4(){
+
+}
